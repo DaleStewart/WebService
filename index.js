8809f3d2406f21188f83980b3632e1ef253fbc45
DaleStewart <97stewart@gmail.com>
2023-06-12 15:36:13 -0400

    Added DB connection Not successful yet

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
 
+//data base vars
+var ibmdb = require("ibm_db") //require("ibm_db")
+  , conn = new ibmdb.Database()
+  , cn = "database=bludb;hostname=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;port=32304;uid=jhk43616;pwd=XHSgreteeb64OhPg";
+
 //port setup
 const PORT = 8080;
 
@@ -15,6 +20,34 @@ app.use(bodyParser.json());
 app.get('/status', (req, res) => {
     const somee = { msg: 'success', status: 'okay' }
     res.send(somee);
+})
+
+app.get('/testdb', (req, res) => {
+    // open a connection to the database
+    conn.openSync(cn);
+    // Select data from table
+    conn.queryResult("select * from client", function (err, result) {
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        // Fetch single row at once and process it.
+        // Note that queryResult will bring only 64k data from server and result.fetchSync
+        // will return each row from this 64k client buffer. Once all data is read from
+        // buffer, ibm_db driver will bring another 64k chunk of data from server.
+        var data;
+        while( data = result.fetchSync() )
+        {
+            console.log(data);
+        }
+
+        // drop the table and close connection.
+        conn.querySync("drop table mytab");
+        conn.closeSync();
+        res.send(data);
+    });
+  
 })
 
 app.get('', (req, res) => {
