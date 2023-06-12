ea0fff66e84f02fc027933a171fad11a0a9a579d
DaleStewart <97stewart@gmail.com>
2023-06-12 16:55:34 -0400

    Debugged connection issues: Always check the password/credentials are in date. Always ensure the parameters are named correctly.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 //data base vars
 var ibmdb = require("ibm_db") //require("ibm_db")
   , conn = new ibmdb.Database()
-  , cn = "database=bludb;hostname=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;port=32304;uid=jhk43616;pwd=XHSgreteeb64OhPg;ssl=true";
+  , cn = "database=bludb;hostname=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;port=32304;uid=jhk43616;pwd=0eVaH3ipUpwy4hcg;SECURITY=SSL";
 
 //port setup
 const PORT = 8080;
